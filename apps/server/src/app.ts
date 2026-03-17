03fd0ec46c140d94aa4eaa1eabc5ea7712c19776
Ihar Shumilouski <ishumilouski@MacBook-Pro-Sumilovskij.local>
2026-03-17 22:47:41 +0300

    feat(auth): add common error handlers

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,11 +1,13 @@
 import fastifyAutoload from "@fastify/autoload";
 import fastify from "fastify";
 import {
+	hasZodFastifySchemaValidationErrors,
 	serializerCompiler,
 	validatorCompiler,
 	type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import path from "node:path";
+import { AppError } from "./utils/errors";
 
 export const buildApp = async () => {
 	const app = fastify({
@@ -18,6 +20,33 @@ export const buildApp = async () => {
 
 	app.setValidatorCompiler(validatorCompiler);
 	app.setSerializerCompiler(serializerCompiler);
+
+	app.setErrorHandler((error, request, reply) => {
+		if (error instanceof AppError) {
+			return reply.status(error.statusCode).send({
+				code: error.statusCode,
+				message: error.message,
+			});
+		}
+
+		if (hasZodFastifySchemaValidationErrors(error)) {
+			return reply.status(400).send({
+				code: "VALIDATION_ERROR",
+				message: "Validation error",
+				details: error.validation.map((v) => ({
+					path: v.instancePath,
+					message: v.message,
+				})),
+			});
+		}
+
+		request.log.error(error);
+
+		return reply.status(500).send({
+			code: "INTERNAL_SERVER_ERROR",
+			message: "Something went wrong",
+		});
+	});
 
 	await app.register(fastifyAutoload, {
 		dir: path.join(__dirname, "plugins"),
